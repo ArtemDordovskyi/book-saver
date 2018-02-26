a44cbb4da66d62c5cc307f41bfa78a776dad6352
artemdordovskyi <dordovskyartem@gmail.com>
2018-02-27 00:46:36 +0200

    Add ability to save file in txt

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ end
 class BookSaver < Sinatra::Base
   get '/download' do
     loveread = Loveread.new(params[:book_name], params[:book_id], params[:author])
-    send_file loveread.send(params[:format])
+    loveread.send(params[:format])
+    content_type "application/#{params[:format]}"
+    attachment "./#{loveread.title(1, false)}.#{params[:format]}"
+    send_file "./#{loveread.title(1, false)}.#{params[:format]}"
   end
 
   get '/' do
@@ -21,6 +24,8 @@ class BookSaver < Sinatra::Base
       begin
         loveread = Loveread.new(@book_name, params[:book_id], params[:author])
         @title = loveread.title(params[:p])
+        @book_id = loveread.book_id
+        @author = loveread.author
         @html_page = loveread.html_page(params[:p])
       rescue
         @html_page = 'Что-то пошло не так'
